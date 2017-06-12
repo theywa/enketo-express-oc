5da81e579b1d0f121a07516f85df48360a8babcb
Made Widiana <theywaquarius@gmail.com>
2017-06-12 19:28:52 +0700

    update: keyboard tabbing reverse on radio button

--- public/js/src/module/radio-tab.js
+++ public/js/src/module/radio-tab.js
@@ -4,12 +4,17 @@ var $ = require( 'jquery' );
 
 $(document).keydown(function(e) {
     if (e.key === 'Tab') {
-        let currentFocus = $(':focus');
+        var currentFocus = $(':focus');
         if (currentFocus.is('input[type=radio]')) {
-            let name = currentFocus.attr('name');
-            let allRadiosSameName = $('input[type=radio][name="' + name +  '"]');
-            let currentRadio = allRadiosSameName.index(currentFocus);
-            let nextRadio = allRadiosSameName[currentRadio + 1];
+            var name = currentFocus.attr('name');
+            var allRadiosSameName = $('input[type=radio][name="' + name +  '"]');
+            var currentRadio = allRadiosSameName.index(currentFocus);
+            var nextRadio;
+            if (!e.shiftKey) {
+                nextRadio = allRadiosSameName[currentRadio + 1];
+            } else {
+                nextRadio = allRadiosSameName[currentRadio - 1];
+            }
             if (nextRadio) {
                 nextRadio.focus();
                 e.preventDefault();
