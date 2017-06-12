0664a17b53cd3ad0985646f48b613af3504716c0
Martijn van de Rijdt <martijn@enketo.org>
2017-06-12 12:59:14 -0600

    fixed: jsbeautification issue

--- public/js/src/module/radio-tab.js
+++ public/js/src/module/radio-tab.js
@@ -2,23 +2,23 @@
 
 var $ = require( 'jquery' );
 
-$(document).keydown(function(e) {
-    if (e.key === 'Tab') {
-        var currentFocus = $(':focus');
-        if (currentFocus.is('input[type=radio]')) {
-            var name = currentFocus.attr('name');
-            var allRadiosSameName = $('input[type=radio][name="' + name +  '"]');
-            var currentRadio = allRadiosSameName.index(currentFocus);
+$( document ).keydown( function( e ) {
+    if ( e.key === 'Tab' ) {
+        var currentFocus = $( ':focus' );
+        if ( currentFocus.is( 'input[type=radio]' ) ) {
+            var name = currentFocus.attr( 'name' );
+            var allRadiosSameName = $( 'input[type=radio][name="' + name + '"]' );
+            var currentRadio = allRadiosSameName.index( currentFocus );
             var nextRadio;
-            if (!e.shiftKey) {
-                nextRadio = allRadiosSameName[currentRadio + 1];
+            if ( !e.shiftKey ) {
+                nextRadio = allRadiosSameName[ currentRadio + 1 ];
             } else {
-                nextRadio = allRadiosSameName[currentRadio - 1];
+                nextRadio = allRadiosSameName[ currentRadio - 1 ];
             }
-            if (nextRadio) {
+            if ( nextRadio ) {
                 nextRadio.focus();
                 e.preventDefault();
             }
         }
     }
-});
+} );
