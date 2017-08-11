ce046e6985b9d3e8eb394f70023db89246e33727
Martijn van de Rijdt <martijn@enketo.org>
2017-08-11 16:47:54 -0600

    changed: OC: do not hide irrelevant groups with values in them,
    closes https://github.com/kobotoolbox/enketo-express/issues/744

--- public/js/src/module/branch.js
+++ public/js/src/module/branch.js
@@ -6,6 +6,25 @@ var branchModule = require( 'enketo-core/src/js/branch' );
 var $ = require( 'jquery' );
 
 /**
+ * Overwrite core functionality by **always** adding 
+ * .or-group.invalid-relevant and .or-group-data.invalid-relevant.
+ */
+branchModule.update = function( updated, forceClearIrrelevant ) {
+    var $nodes;
+
+    if ( !this.form ) {
+        throw new Error( 'Branch module not correctly instantiated with form property.' );
+    }
+
+    $nodes = this.form.getRelatedNodes( 'data-relevant', '', updated )
+        // the OC customization:
+        .add( this.form.getRelatedNodes( 'data-relevant', '.invalid-relevant' ) );
+
+    this.updateNodes( $nodes, forceClearIrrelevant );
+};
+
+
+/**
  * Overwrite core functionality by removing isSelfRelevant check
  */
 branchModule.enable = function( $branchNode ) {
@@ -16,7 +35,9 @@ branchModule.enable = function( $branchNode ) {
 };
 
 
-// Overwrite clear function
+/**
+ * Overwrite clear function
+ */
 branchModule.clear = function() {
     // Only user can clear values in OC.
 };
@@ -39,8 +60,9 @@ branchModule.activate = function( $branchNode ) {
         this.form.validateInput( $control );
         if ( required ) {
             $control.attr( 'data-required', required );
-
         }
+    } else if ( $branchNode.is( '.or-group, .or-group-data' ) ) {
+        this.form.setValid( $branchNode, 'relevant' );
     }
 };
 
@@ -52,6 +74,7 @@ branchModule.deactivate = function( $branchNode ) {
     var index = 0;
     var value;
     var $control;
+    var that = this;
 
     if ( $branchNode.is( '.question' ) ) {
         $control = $( $branchNode[ 0 ].querySelector( 'input, select, textarea' ) );
@@ -72,9 +95,37 @@ branchModule.deactivate = function( $branchNode ) {
             $branchNode.trigger( 'hiding.oc' );
         }
 
-    } else {
-        //TODO: if group descendent has a value, add the relevantError class and remove other errors
-        this.originalDeactivate( $branchNode );
-        $branchNode.trigger( 'hiding.oc' );
+    } else if ( $branchNode.is( '.or-group, .or-group-data' ) ) {
+        name = this.form.input.getName( $branchNode );
+        index = this.form.input.getIndex( $branchNode );
+        /*
+         * We need to check if any of the _regular_ fields with a form control 
+         * (ie. excl calculations and discrepancy note questions) has a value.
+         * The best way is to do this in the model.
+         * 
+         * First get all the leafnodes (nodes without children) and then check if there is a calculation 
+         * or dn question for this node.
+         * 
+         * Then get the concatenated textContent of the filtered leafnodes and trim to avoid 
+         * recognizing whitespace-only as a value. (whitespace in between is fine as it won't give a false positive)
+         * 
+         * If the result has length > 0, one form control in the group has a value.
+         */
+        value = this.form.model.node( name, index ).get().find( '*' ).filter( function() {
+            if ( $( this ).children().length === 0 ) {
+                var path = that.form.model.getXPath( this, 'instance' );
+                var $n = that.form.view.$.find( '.calculation > [name="' + path + '"], .or-appearance-dn > [name="' + path + '"]' );
+                return $n.length === 0;
+            }
+            return false;
+        } ).text().trim();
+
+        if ( value.length ) {
+            this.form.setInvalid( $branchNode, 'relevant' );
+        } else {
+            this.form.setValid( $branchNode, 'relevant' );
+            this.originalDeactivate( $branchNode );
+            $branchNode.trigger( 'hiding.oc' );
+        }
     }
 };
--- test/client/branch.spec.js
+++ test/client/branch.spec.js
@@ -15,134 +15,219 @@ var loadForm = function( filename ) {
 };
 
 describe( 'Customized Branching Logic', function() {
-    var a = '[name="/relevant-constraint-required/something"]';
-    var b = '[name="/relevant-constraint-required/rep/val"]';
-    var c = '[name="/relevant-constraint-required/rep/skipq"]';
+    var a = '[name="/data/something"]';
+    var b = '[name="/data/rep/val"]';
+    var c = '[name="/data/rep/skipq"]';
+    var r = '[name="/data/rep"]';
 
-    describe( 'disabled class', function() {
-        var form;
+    describe( 'for individual questions', function() {
 
-        beforeEach( function() {
-            form = loadForm( 'relevant_constraint_required.xml' );
-            form.init();
-            form.view.$.find( a ).val( 'a' ).trigger( 'change' );
-            form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+        describe( 'disabled class', function() {
+            var form;
+
+            beforeEach( function() {
+                form = loadForm( 'relevant_constraint_required.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+            } );
+
+            // Test if we haven't messed up Enketo Core's default functionality
+            it( 'is still added if value is empty', function() {
+                // not disabled
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false );
+                // make c irrelevant
+                form.view.$.find( b ).val( 'd' ).trigger( 'change' );
+                // check if disabled
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( true );
+            } );
+
+            // Test OC-custom functionality
+            it( 'is not added if value is non-empty (1)', function() {
+                // add value to c
+                form.view.$.find( c ).val( 5 ).trigger( 'change' );
+                // not disabled
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false );
+                // make c irrelevant
+                form.view.$.find( b ).val( 'd' ).trigger( 'change' );
+                // check if disabled
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+            } );
+
+            // Test OC-custom functionality
+            it( 'is not added if value is non-empty (2)', function() {
+                // add value to c
+                form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
+                // check if disabled
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+            } );
+
         } );
 
-        // Test if we haven't messed up Enketo Core's default functionality
-        it( 'is still added if value is empty', function() {
-            // not disabled
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false );
-            // make c irrelevant
-            form.view.$.find( b ).val( 'd' ).trigger( 'change' );
-            // check if disabled
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( true );
+        describe( 'with relevant error', function() {
+            var form;
+
+            beforeEach( function() {
+                form = loadForm( 'relevant_constraint_required.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+                // add value to c
+                form.view.$.find( c ).val( 5 ).trigger( 'change' );
+                // make c irrelevant
+                form.view.$.find( b ).val( 'd' ).trigger( 'change' );
+            } );
+
+            it( 'shown to alert user that value is non-empty (1)', function() {
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+            } );
+
+            it( 'shown to alert user that value is non-empty (2)', function() {
+                form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
+                expect( form.view.$.find( b ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+            } );
+
+            it( 'removed if value changes from non-empty to empty', function() {
+                // remove value for c
+                form.view.$.find( c ).val( '' ).trigger( 'change' );
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( false );
+            } );
         } );
 
-        // Test OC-custom functionality
-        it( 'is not added if value is non-empty (1)', function() {
-            // add value to c
-            form.view.$.find( c ).val( 5 ).trigger( 'change' );
-            // not disabled
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false );
-            // make c irrelevant
-            form.view.$.find( b ).val( 'd' ).trigger( 'change' );
-            // check if disabled
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+
+        describe( 'with relevant error interacting with constraint error', function() {
+            var form;
+
+            beforeEach( function( done ) {
+                form = loadForm( 'relevant_constraint_required.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'afdgsgsfafdfadssf' ).trigger( 'change' );
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+                // add value to c that fails constraint
+                form.view.$.find( c ).val( 5 ).trigger( 'change' );
+                // make c irrelevant (and still failing constraint validation too)
+                form.view.$.find( b ).val( 'diarrheadafsdsfdasd' ).trigger( 'change' );
+                setTimeout( function() {
+                    done();
+                }, 500 );
+            } );
+
+            it( 'shows relevant error but not constraint error when form.validate() is called', function() {
+                return form.validate()
+                    .then( function( result ) {
+                        return Promise.all( [
+                            expect( result ).to.equal( false ),
+                            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true ),
+                            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-constraint' ) ).to.equal( false ),
+                        ] );
+                    } );
+            } );
+
         } );
 
-        // Test OC-custom functionality
-        it( 'is not added if value is non-empty (2)', function() {
-            // add value to c
-            form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
-            // check if disabled
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+        describe( 'resets relevantError', function() {
+            var form;
+
+            beforeEach( function( done ) {
+                form = loadForm( 'relevant_constraint_required.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                form.view.$.find( b ).val( 'd' ).trigger( 'change' );
+                // make b irrelevant
+                form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
+                setTimeout( function() {
+                    done();
+                }, 500 );
+            } );
+
+            it( 'when question becomes relevant again', function() {
+                expect( form.view.$.find( b ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+                // make b relevant again
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( false );
+            } );
         } );
 
     } );
 
-    describe( 'with relevant error', function() {
-        var form;
 
-        beforeEach( function() {
-            form = loadForm( 'relevant_constraint_required.xml' );
-            form.init();
-            form.view.$.find( a ).val( 'a' ).trigger( 'change' );
-            form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
-            // add value to c
-            form.view.$.find( c ).val( 5 ).trigger( 'change' );
-            // make c irrelevant
-            form.view.$.find( b ).val( 'd' ).trigger( 'change' );
+    describe( 'for groups', function() {
+
+        describe( 'disabled class', function() {
+            var form;
+
+            beforeEach( function() {
+                form = loadForm( 'relevant_group.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+            } );
+
+            // Test if we haven't messed up Enketo Core's default functionality
+            it( 'is still added if value is empty', function() {
+                // r not disabled
+                expect( form.view.$.find( r ).hasClass( 'disabled' ) ).to.equal( false );
+                // make r irrelevant (no values inside repeat)
+                form.view.$.find( a ).val( '' ).trigger( 'change' );
+                // r now disabled
+                expect( form.view.$.find( r ).hasClass( 'disabled' ) ).to.equal( true );
+            } );
+
+            // Test OC-custom functionality
+            it( 'is not added if a user-entered value inside group is non-empty', function() {
+                // add value inside repeat
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+                // make r irrelevant (while it contains value)
+                form.view.$.find( a ).val( '' ).trigger( 'change' );
+                // r not disabled
+                expect( form.view.$.find( r ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+            } );
+
+            // Test OC-custom functionality
+            it( 'is not added if multiple user-entered values are non-emptys', function() {
+                // add value to b
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+                // add value to c
+                form.view.$.find( c ).val( '4' ).trigger( 'change' );
+                // check if disabled
+                expect( form.view.$.find( r ).hasClass( 'disabled' ) ).to.equal( false ); // FALSE!
+            } );
+
         } );
 
-        it( 'shown to alert user that value is non-empty (1)', function() {
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+        describe( 'with relevant error', function() {
+            var form;
+
+            beforeEach( function() {
+                form = loadForm( 'relevant_group.xml' );
+                form.init();
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
+                // add value to c
+                form.view.$.find( c ).val( 5 ).trigger( 'change' );
+                // make r irrelevant
+                form.view.$.find( a ).val( '' ).trigger( 'change' );
+            } );
+
+            it( 'shown to alert user that a user-entered value is non-empty', function() {
+                expect( form.view.$.find( r ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+            } );
+
+            it( 'removed if group becomes relevant again', function() {
+                // remove one value
+                form.view.$.find( a ).val( 'a' ).trigger( 'change' );
+                expect( form.view.$.find( r ).hasClass( 'invalid-relevant' ) ).to.equal( false );
+            } );
+
+            it( 'removed when all user-entered values have changed from non-empty to empty', function() {
+                // remove one value
+                form.view.$.find( c ).val( '' ).trigger( 'change' );
+                expect( form.view.$.find( r ).hasClass( 'invalid-relevant' ) ).to.equal( true );
+                // remove remaining value
+                form.view.$.find( b ).val( '' ).trigger( 'change' );
+                expect( form.view.$.find( r ).hasClass( 'invalid-relevant' ) ).to.equal( false );
+            } );
+
         } );
 
-        it( 'shown to alert user that value is non-empty (2)', function() {
-            form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
-            expect( form.view.$.find( b ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
-        } );
-
-        it( 'removed if value changes from non-empty to empty', function() {
-            // remove value for c
-            form.view.$.find( c ).val( '' ).trigger( 'change' );
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( false );
-        } );
     } );
-
-
-    describe( 'with relevant error interacting with constraint error', function() {
-        var form;
-
-        beforeEach( function( done ) {
-            form = loadForm( 'relevant_constraint_required.xml' );
-            form.init();
-            form.view.$.find( a ).val( 'afdgsgsfafdfadssf' ).trigger( 'change' );
-            form.view.$.find( b ).val( 'diarrhea' ).trigger( 'change' );
-            // add value to c that fails constraint
-            form.view.$.find( c ).val( 5 ).trigger( 'change' );
-            // make c irrelevant (and still failing constraint validation too)
-            form.view.$.find( b ).val( 'diarrheadafsdsfdasd' ).trigger( 'change' );
-            setTimeout( function() {
-                done();
-            }, 500 );
-        } );
-
-        it( 'shows relevant error but not constraint error when form.validate() is called', function() {
-            return form.validate()
-                .then( function( result ) {
-                    return Promise.all( [
-                        expect( result ).to.equal( false ),
-                        expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true ),
-                        expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-constraint' ) ).to.equal( false ),
-                    ] );
-                } );
-        } );
-
-    } );
-
-    describe( 'resets relevantError', function() {
-        var form;
-
-        beforeEach( function( done ) {
-            form = loadForm( 'relevant_constraint_required.xml' );
-            form.init();
-            form.view.$.find( a ).val( 'a' ).trigger( 'change' );
-            form.view.$.find( b ).val( 'd' ).trigger( 'change' );
-            // make b irrelevant
-            form.view.$.find( a ).val( 'nothing' ).trigger( 'change' );
-            setTimeout( function() {
-                done();
-            }, 500 );
-        } );
-
-        it( 'when question becomes relevant again', function() {
-            expect( form.view.$.find( b ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( true );
-            // make b relevant again
-            form.view.$.find( a ).val( 'a' ).trigger( 'change' );
-            expect( form.view.$.find( c ).closest( '.question' ).hasClass( 'invalid-relevant' ) ).to.equal( false );
-        } );
-    } );
-
 } );
