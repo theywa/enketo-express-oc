ce692daa5614928b10da9ee46b2ce9f4d080ce06
Martijn van de Rijdt <martijn@enketo.org>
2017-08-28 15:03:14 -0600

    changed: for complete records let Close button function as Complete button,
    https://github.com/kobotoolbox/enketo-express/issues/724

--- public/js/src/module/controller-webform-fieldsubmission.js
+++ public/js/src/module/controller-webform-fieldsubmission.js
@@ -310,6 +310,10 @@ function _setReasonForChangeUi() {
 function _removeCompleteButtonIfNeccessary() {
     if ( settings.type === 'edit' && !settings.completeButton ) {
         $( 'button#finish-form' ).remove();
+        console.log( 'MAGIC!' );
+        // Now, let the Close button function as a Complete button...
+        // Yes, this is a mess. It is supposed to be only temporary.
+        $( 'button#close-form' ).attr( 'id', 'finish-form' );
     }
 }
 
