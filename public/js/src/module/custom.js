197d2c78511b82155275d5abdfb8af60d4d569fc
Martijn van de Rijdt <martijn@enketo.org>
2017-11-02 14:51:54 -0600

    changed: OC: show full signature string as provided in instance,
    closes https://github.com/kobotoolbox/enketo-express/issues/843

--- public/js/src/module/custom.js
+++ public/js/src/module/custom.js
@@ -3,21 +3,15 @@
 // Custom OC things used across views
 
 var $ = require( 'jquery' );
-var t = require( 'translator' ).t;
 
 function addSignedStatus( form ) {
     var $status;
     var metaPlus = '/*/' /* + model.getNamespacePrefix( 'http://openrosa.org/xforms' ) + ':'*/ +
         'meta/' + form.model.getNamespacePrefix( 'http://openclinica.org/xforms' ) + ':';
-    var signedBy = form.model.evaluate( metaPlus + 'signedBy', 'string' );
-    var signedOn = form.model.evaluate( metaPlus + 'signedOn', 'string' );
-
-    if ( signedBy && signedOn ) {
-        $status = $( '<div class="record-signed-status">' + t( 'signed.msg', {
-                name: signedBy,
-                date: signedOn
-            } ) +
-            '</div>' );
+    var signature = form.model.evaluate( metaPlus + 'signature', 'string' );
+    if ( signature ) {
+        $status = $( '<div class="record-signed-status">' +
+            signature.replace( /\\n/g, '<br/>' ).replace( /\n/g, '<br/>' ) + '</div>' );
         $( '#form-title' )
             .before( $status )
             .closest( 'form.or' ).on( 'valuechange.enketo inputupdate.enketo', function() {
