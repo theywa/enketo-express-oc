64e9d1e973082689211f483477e2bc709e577ab2
Martijn van de Rijdt <martijn@enketo.org>
2018-05-10 11:57:44 -0600

    added: OC: create release package, closes https://github.com/kobotoolbox/enketo-express/issues/997

---
+++ tools/release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+OS=$TRAVIS_OS_NAME
+echo "Creating release in environment: Node version: ${TRAVIS_NODE_VERSION} for tag: ${TRAVIS_TAG} in ${OS}..."
+
+TARGET_BASE="../enketo-express-packages"
+FILENAME="enketo-express-oc"
+
+# Clean up node_modules to reduce zip file size
+npm prune --production
+
+# Create the directory 
+mkdir -p ${TARGET_BASE}
+
+# Create zip file:
+cd ${TARGET_BASE}
+zip -rq9 ${FILENAME}_node${TRAVIS_NODE_VERSION}_${OS}.zip ../enketo-express-oc -x "*.git*" "*/public/js/src/*" "*/public/js/*-bundle.js" "*/setup/*" "*/test/*" "*/doc/*"
