0d10cd2a1349f4b5febb6dac9cbaaedb37666f37
Martijn van de Rijdt <martijn@enketo.org>
2018-06-08 14:03:00 -0600

    added: OC: write git tag to file as part of release build script

--- tools/release.sh
+++ tools/release.sh
@@ -11,6 +11,9 @@ npm prune --production
 # Create the directory 
 mkdir -p ${TARGET_BASE}
 
+# Create a file containing the git tag
+echo ${TRAVIS_TAG} > .tag.txt
+
 # Create zip file:
 cd ${TARGET_BASE}
 zip -rq9 ${FILENAME}_node${TRAVIS_NODE_VERSION}_${OS}.zip ../enketo-express-oc -x "*.git*" "*/public/js/src/*" "*/public/js/*-bundle.js" "*/setup/*" "*/test/*" "*/doc/*"
