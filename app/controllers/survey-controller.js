b66876d04172b4eb25d16ddca8d7ec849da671cf
Martijn van de Rijdt <martijn@enketo.org>
2018-09-17 13:06:11 -0600

    fixed: OC: complete button not shown in regular /survey/collect/ (empty fs form), closes #107

--- app/controllers/survey-controller.js
+++ app/controllers/survey-controller.js
@@ -45,7 +45,7 @@ router
     .get( '*/headless*', _setHeadless )
     .get( '/preview*', _setJini )
     .get( /\/(single|edit)\/fs(\/rfc)?(\/c)?\/i/, _setJini )
-    .get( /\/edit\/fs\/(?!(participant|rfc))/, _setCompleteButton )
+    .get( /\/(edit|single)\/fs\/(?!(participant|rfc))/, _setCompleteButton )
     .get( '*', _setCloseButtonClass )
     .get( '/:enketo_id', webform )
     .get( '/:mod/:enketo_id', webform )
