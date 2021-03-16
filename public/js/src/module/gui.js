4f708b0c0d39a276b8ae85655d17e223b07087ba
dewa <theywaquarius@gmail.com>
2021-03-16 15:07:00 +0800

    fixed: OC: Issue With Printing Single Select Items with Minimal Display on Chrome and Edge

--- public/js/src/module/gui.js
+++ public/js/src/module/gui.js
@@ -564,22 +564,26 @@ function getPrintDialogComponents() {
     };
 }
 
-function printGrid( format ) {
+function printGrid( format, delay = 800 ) {
     const swapped = printHelper.styleToAll();
-
-    return printHelper.fixGrid( format )
-        .then( window.print )
-        .catch( console.error )
-        .then( () => {
-            if ( swapped ) {
-                return new Promise( resolve => {
-                    setTimeout( () => {
-                        printHelper.styleReset();
-                        resolve();
-                    }, 500 );
+    // https://github.com/OpenClinica/enketo-express-oc/issues/456
+    // changing style on chrome takes longer than other browser
+    return new Promise( resolve => setTimeout( () => {
+            printHelper.fixGrid( format )
+                .then( window.print )
+                .catch( console.error )
+                .then( () => {
+                    if ( swapped ) {
+                        return new Promise( resolve => {
+                            setTimeout( () => {
+                                printHelper.styleReset();
+                                resolve();
+                            }, 500 );
+                        } );
+                    }
                 } );
-            }
-        } );
+            resolve();
+        }, delay ) );
 }
 
 /**
